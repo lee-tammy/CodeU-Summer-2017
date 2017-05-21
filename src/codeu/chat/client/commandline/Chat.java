4a1b8156fa430ade35ca8028a82fc2cb625f2f77
Liz <liz.weech@gmail.com>
2017-05-21 14:21:05 -0400

    added Tokenizer and TokenizerTest classes
    implemented the code provided in the CodeU tutorial

--- src/codeu/chat/client/commandline/Chat.java
+++ src/codeu/chat/client/commandline/Chat.java
@@ -14,7 +14,9 @@
 
 package codeu.chat.client.commandline;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
@@ -23,6 +25,7 @@ import codeu.chat.client.core.Context;
 import codeu.chat.client.core.ConversationContext;
 import codeu.chat.client.core.MessageContext;
 import codeu.chat.client.core.UserContext;
+import codeu.chat.util.Tokenizer;
 
 public final class Chat {
 
@@ -47,9 +50,13 @@ public final class Chat {
   //
   public boolean handleCommand(String line) {
 
-    final Scanner tokens = new Scanner(line.trim());
-
-    final String command = tokens.hasNext() ? tokens.next() : "";
+	final List<String> args = new ArrayList<>();
+	final Tokenizer tokenizer = new Tokenizer(line);
+	for (String token = tokenizer.next(); token != null; token = tokenizer.next()) {
+	  args.add(token);
+	}
+	final String command = args.get(0);
+	args.remove(0);
 
     // Because "exit" and "back" are applicable to every panel, handle
     // those commands here to avoid having to implement them for each
