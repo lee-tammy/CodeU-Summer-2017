e25a6c3bd9c0e3f1a896db551ce9535fd619d70d
Adam Snaider <adsnaider@gmail.com>
2017-05-25 20:43:00 -0700

    Added duration to up time information.

--- src/codeu/chat/client/commandline/Chat.java
+++ src/codeu/chat/client/commandline/Chat.java
@@ -198,7 +198,8 @@ public final class Chat {
           System.out.println("Server Information:");
           System.out.format("  Start Time : %s\n", info.startTime.toString());
           System.out.format("  Time now   : %s\n", Time.now());
-          // TODO(Adam): Add duration?
+          System.out.format("  Duration   : %s sec\n", Time.duration(info.startTime,
+                Time.now(), 0));
         }
       }
     });
