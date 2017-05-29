a7478dff1e5245b7bb4b1e8b4d300bb0bbdc2302
Tammy Lee <tammylee@Tammys-MacBook-Pro-2.local>
2017-05-28 17:33:55 -0700

    Fixed indentation

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 import codeu.chat.util.Uuid;
 
 public final class ServerInfo {
-	private final static String SERVER_VERSION = "1.0.0";
+  private final static String SERVER_VERSION = "1.0.0";
 
-	public final Uuid version;
-
+  public final Uuid version;
+  
 	public ServerInfo() {
 		Uuid version = null;
 		try {
 			version = Uuid.parse(SERVER_VERSION);
 		} catch (IOException e) {
 			System.out.println("Could not parse version");
+			// should not happen
 		}
 		this.version = version;
 	}
