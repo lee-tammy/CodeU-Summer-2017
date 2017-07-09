32d56b9c5eb86831b3954617675a266a90d65b93
Liz Weech <liz.weech@gmail.com>
2017-07-09 09:51:17 -0400

    changed ConversationPermission to reflect meeting
    tried to use the Store class in codeu.chat.util.store but I'm not convinced it has the functionality we want - not sure if it allows duplicate Uuid key entries or not

--- src/codeu/chat/common/ConversationPermission.java
+++ src/codeu/chat/common/ConversationPermission.java
@@ -3,18 +3,39 @@ package codeu.chat.common;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
 
 import codeu.chat.util.Serializer;
 import codeu.chat.util.Serializers;
 import codeu.chat.util.Uuid;
+import codeu.chat.util.store.Store;
 
 public class ConversationPermission {
+  private static final Comparator<Uuid> UUID_COMPARE = new Comparator<Uuid>() {
+	
+	@Override
+	public int compare(Uuid a, Uuid b) {
+
+	  if (a == b) {
+	    return 0;
+	  }
+
+	  if (a == null && b != null) {
+	    return -1;
+	  }
+
+	  if (a != null && b == null) {
+	    return 1;
+	  }
+
+	  final int order = Integer.compare(a.id(), b.id());
+		return order == 0 ? compare(a.root(), b.root()) : order;
+	  }
+  };
   private Uuid cpID;
-  private LinkedHashSet<Uuid> owners;
-  private LinkedHashSet<Uuid> members;
-  private final Uuid CREATOR;
+  private Store users = new Store(UUID_COMPARE);
 	
   // TODO: properly implement Serializer for ConversationPermission
   public static final Serializer<ConversationPermission> SERIALIZER = new Serializer<ConversationPermission>() {
@@ -28,45 +49,36 @@ public class ConversationPermission {
 	}
   };
   
+  // creator is 3, owner is 2, member is 1, default/undefined is 0
   public ConversationPermission(Uuid creator, Uuid cpID) {
 	this.cpID = cpID;
-	this.CREATOR = creator;
-	owners = new LinkedHashSet<Uuid>();
-	members = new LinkedHashSet<Uuid>();
+	users.insert(creator, 3);
   }
   
-  public boolean isOwner(Uuid user) {
-	return owners.contains(user);
+  public int returnStatus(Uuid user) {
+	return (int)users.first(user);
   }
   
-  public boolean isMember(Uuid user) {
-	return members.contains(user);
+  public void addOwner(Uuid user) {
+    if(users.first(user) != null) {
+      users.insert(user, null);
+    }
+    users.insert(user, 2);
   }
   
-  // returns false if user is already an Owner
-  public boolean addOwner(Uuid user) {
-    members.add(user);
-	return owners.add(user); 
+  public void addMember(Uuid user) {
+	if(users.first(user) != null) {
+	  users.insert(user, null);
+	}
+	  users.insert(user, 1);
   }
   
-  // returns false if user is already a Member
-  public boolean addMember(Uuid user) {
-	return members.add(user);
+  public void removeOwner(Uuid user) {
+	users.insert(user, null);
   }
   
-  // returns false if user was not actually an Owner
-  public boolean removeOwner(Uuid user) {
-	members.remove(user);
-	return owners.remove(user);
-  }
-  
-  // returns false if user was not actually a Member
-  public boolean removeMember(Uuid user) {
-	return members.remove(user);
-  }
-  
-  public Uuid getCreator() {
-	return this.CREATOR;
+  public void removeMember(Uuid user) {
+	users.insert(user, null);
   }
   
   // returns ID of the Conversation Permissions
